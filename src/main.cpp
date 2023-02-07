d1dec710c407f105e1602b4d516f24ac34cb7216
Lukasz <lukasz.sromek@gmail.com>
2023-02-07 11:09:39 +0100

    basic scrolling with sizers

--- src/main.cpp
+++ src/main.cpp
@@ -24,4 +24,16 @@ wxIMPLEMENT_APP(MyApp);
 MyFrame::MyFrame(const wxString &title, const wxPoint &pos, const wxSize &size)
     : wxFrame(NULL, wxID_ANY, title, pos, size)
 {
+    auto panel = new wxScrolled<wxPanel>(this, wxID_ANY);
+    panel->SetScrollRate(0, FromDIP(10));
+
+    auto sizer = new wxBoxSizer(wxVERTICAL);
+
+    for (int i = 0; i < 15; i++)
+    {
+        auto button = new wxButton(panel, wxID_ANY, "Button " + std::to_string(i));
+        sizer->Add(button, 0, wxALIGN_CENTER | wxALL, FromDIP(10));
+    }
+
+    panel->SetSizer(sizer);
 }
